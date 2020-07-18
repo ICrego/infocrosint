5dd9efeec250e19c885e97250e1d9b030fc89943
I.Crego <ignaciocrego@gmail.com>
2020-07-18 17:06:38 +0200

    Delete builtwith.py

--- modulos/builtwith.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def analyze_builtwith(api_key , q , t):
-    res=requests.get('https://api.builtwith.com/v14/api.json?KEY=' + api_key + '&LOOKUP=' + q)
-    
-    todo=res.json()
-    print(todo)
